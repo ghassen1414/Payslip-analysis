7c8e33bbf03ed10d04440df4f11cd30de8ce01ab
Zarrouk <ghassen.zarrouk42@gmail.com>
2025-04-28 14:27:04 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import ListGroup from "./components/ListGroup";
+import List from "./components/ListGroup";
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+export default App;
